86e1748dd9cf7a69440d98fdbe76747a423c46c7
skylerlewis <skylerlewispg@gmail.com>
2026-01-28 11:51:59 -0800

    fix number of locations limit

--- scripts/fetch_hydrovu.R
+++ scripts/fetch_hydrovu.R
@@ -46,47 +46,69 @@ get_access_token <- function() {
 
 ## API HELPERS ================================================================
 
-get_locations <- function(token) {
-  GET(paste0(BASE_API, "/locations/list"),
-      add_headers(Authorization = paste("Bearer", token))) |>
-    content("text", encoding = "UTF-8") |>
-    fromJSON(flatten = TRUE) |>
-    as_tibble()
-}
-
-get_parameter_names <- function(token) {
-  GET(paste0(BASE_API, "/sispec/friendlynames"),
-      add_headers(Authorization = paste("Bearer", token))) |>
-    content("parsed", type = "application/json") |>
-    (\(x) x$parameters)() |>
-    enframe(name = "parm_id", value = "parm_name") |>
-    unnest(parm_name)
-}
-
-get_readings_paginated <- function(token, location_id, parameter_df,
-                                   start_ts, end_ts) {
-  
-  start_str <- format(start_ts, "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
-  end_str   <- format(end_ts,   "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
-  
-  pages <- list()
+isi_paginate <- function(token, url, query = NULL, parse_as="text", body_parse = function(x) x) {
+  out <- list()
   next_page <- NULL
   
   repeat {
     hdrs <- c(Authorization = paste("Bearer", token))
     if (!is.null(next_page)) hdrs["X-ISI-Start-Page"] <- next_page
     
-    res <- GET(
-      paste0(BASE_API, "/locations/", location_id, "/data"),
-      add_headers(.headers = hdrs),
-      query = list(startDate = start_str, endDate = end_str)
-    )
+    # Read as text
+    res <- GET(url, add_headers(.headers = hdrs), query = query)
     stop_for_status(res)
+    parsed_body <- content(res, parse_as, encoding = "UTF-8")
     
-    parms <- content(res, "parsed", type = "application/json")$parameters
+    # Parse via jsonlite
+    out[[length(out) + 1]] <- body_parse(parsed_body)
     
-    if (length(parms) > 0) {
-      df <- parms |>
+    next_page <- headers(res)[["x-isi-next-page"]]
+    if (is.null(next_page) || next_page == "") break
+  }
+  
+  bind_rows(out)
+}
+
+get_locations <- function(token) {
+  isi_paginate(
+    token,
+    paste0(BASE_API, "/locations/list"),
+    parse_as = "text",
+    body_parse = function(body) fromJSON(body, flatten = TRUE) |> as_tibble()
+  )
+}
+
+get_parameter_names <- function(token) {
+  res <- GET(paste0(BASE_API, "/sispec/friendlynames"),
+             add_headers(Authorization = paste("Bearer", token)))
+  stop_for_status(res)
+  
+  txt <- content(res, "text", encoding = "UTF-8")
+  parsed <- fromJSON(txt, flatten = TRUE)
+  
+  # Convert parameters list to tibble
+  parsed$parameters |>
+    enframe(name = "parm_id", value = "parm_name") |>
+    unnest(parm_name)
+}
+
+get_readings <- function(token, location_id, parameter_df,
+                         start_ts, end_ts) {
+  start_str <- format(start_ts, "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
+  end_str   <- format(end_ts,   "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")
+  
+  query <- list(startDate = start_str, endDate = end_str)
+  url <- paste0(BASE_API, "/locations/", location_id, "/data")
+  
+  isi_paginate(
+    token,
+    url,
+    query = query,
+    parse_as = "parsed",
+    body_parse = function(body) {
+      parms <- body$parameters
+      if (length(parms) == 0) return(NULL)
+      parms |>
         enframe() |>
         unnest_wider(value) |>
         select(parameterId, unitId, readings) |>
@@ -94,15 +116,8 @@ get_readings_paginated <- function(token, location_id, parameter_df,
         unnest(readings) |>
         unnest_wider(readings) |>
         mutate(timestamp = as_datetime(as.numeric(timestamp), tz = "UTC"))
-      
-      pages[[length(pages) + 1]] <- df
     }
-    
-    next_page <- headers(res)[["x-isi-next-page"]]
-    if (is.null(next_page)) break
-  }
-  
-  bind_rows(pages)
+  )
 }
 
 ## LOAD EXISTING DATA ==========================================================
@@ -130,14 +145,25 @@ message("Fetching data from ", start_ts, " to ", end_ts)
 ## FETCH NEW DATA ==============================================================
 
 token <- get_access_token()
-locs  <- get_locations(token)
+locs  <- get_locations(token) |>
+  filter(name %in% gages$name)
 parms <- get_parameter_names(token)
+
+################################################################################
+# # temporary patch for missing locations
+# existing_locs <- existing_data |> 
+#   group_by(id, name, description) |> 
+#   summarize(.groups = "drop")
+# 
+# locs <- locs |> 
+#   full_join(existing_locs, by = join_by(id, name, description))
+################################################################################
 
 new_data <- locs |>
   inner_join(gages, by = join_by(name == name)) |>
   mutate(result = map(
     id,
-    ~ get_readings_paginated(token, .x, parms, start_ts, end_ts)
+    ~ get_readings(token, .x, parms, start_ts, end_ts)
   )) |>
   unnest(result) |>
   mutate(
